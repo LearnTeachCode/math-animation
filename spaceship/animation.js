cf3849463fd62d72c75090683f03f25d0a06bcd7
Jim Thoburn <jim@jimthoburn.com>
2016-09-10 22:54:37 -0700

    Reorganize JavaScript.

--- spaceship/animation.js
+++ spaceship/animation.js
@@ -1,49 +1,14 @@
-
-
-	// http://upshots.org/actionscript/jsas-understanding-easing
-	/*
-	@t is the current time (or position) of the tween. This can be seconds or frames, steps, seconds, ms, whatever – as long as the unit is the same as is used for the total time [3].
-	@b is the beginning value of the property.
-	@c is the change between the beginning and destination value of the property.
-	@d is the total time of the tween.
-	*/
-	function noEasing (t, b, c, d) {
-		return c * (t / d) + b;
-	}
-
-	// http://gsgd.co.uk/sandbox/jquery/easing/
-	function easeOutElastic (t, b, c, d) {
-		var s=1.70158;var p=0;var a=c;
-		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-		if (a < Math.abs(c)) { a=c; var s=p/4; }
-		else var s = p/(2*Math.PI) * Math.asin (c/a);
-		return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
-	}
-
-	function easeInQuint (t, b, c, d) {
-		return c*(t/=d)*t*t*t*t + b;
-	}
-
-	// KUDOS: https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
-	function getScrollY() {
-		var supportPageOffset = window.pageXOffset !== undefined;
-		var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
-		return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
-	}
+(function() {
 
 	var spaceship = document.getElementById('spaceship');
 	var bridgeshape = document.getElementById('bridgeshape');
-
-	var mouseY;
-	var beginning = null;
-
-	var readyForLaunch = false;
 
 	var SPACESHIP_START = 99;
 	var SPACESHIP_END = 154;
 
 	var BRIDGESHAPE_START = 80;
 	var BRIDGESHAPE_END = 130;
+
 
 	var setSpaceshipPosition, getSpaceshipPosition;
 	(function() {
@@ -70,60 +35,64 @@
 	})();
 
 
-	function step(mouseY) {
-		if (!beginning) beginning = mouseY;
+	function addListeners() {
+		var mouseIsDown = false;
 
-		var propertyStart, propertyDestination, nextValue;
-		var progress = mouseY - beginning;
-		var duration = 200;
+		document.body.addEventListener('mousedown', function(event) {
+			mouseIsDown = true;
+		}, false);
+		document.body.addEventListener('mouseup', function(event) {
+			mouseIsDown = false;
 
-		readyForLaunch = false;
+			// Animate things back to the top position
+			animate(getBridgeshapePosition(), BRIDGESHAPE_START, 1000, setBridgeshapePosition);
+			animate(getSpaceshipPosition(), SPACESHIP_START, 1000, setSpaceshipPosition);
+		}, false);
 
-		// If we're done
-		if (progress > duration) {
-			readyForLaunch = true;
-			return;
-		}
-
-		// Animate the bridge
-		propertyStart = BRIDGESHAPE_START;
-		propertyDestination = BRIDGESHAPE_END;
-		nextValue = Math.floor(easeInQuint(progress, propertyStart, propertyDestination - propertyStart, duration));
-		setBridgeshapePosition(nextValue);
-
-		// Animate the spaceship
-		propertyStart = SPACESHIP_START;
-		propertyDestination = SPACESHIP_END;
-		nextValue = Math.floor(easeInQuint(progress, propertyStart, propertyDestination - propertyStart, duration));
-		setSpaceshipPosition(nextValue);
-
-		console.log('--------------');
-		console.log('beginning: ' + beginning);
-		console.log('mouseY: ' + mouseY);
-		console.log('progress: ' + progress);
-		console.log('nextValue: ' + nextValue);
+		document.body.addEventListener('mousemove', function(event) {
+			var mouseY = event.clientY;
+			console.log('mouseY: ' + mouseY);
+			if (mouseIsDown) {
+				animateByMouse(mouseY);
+			}
+		}, false);
 	}
 
-	var mouseIsDown = false;
-	document.body.addEventListener('mousedown', function(event) {
-		mouseIsDown = true;
-	}, false);
-	document.body.addEventListener('mouseup', function(event) {
-		mouseIsDown = false;
 
-		// Animate things back to the top position
-		animate(getBridgeshapePosition(), BRIDGESHAPE_START, 1000, setBridgeshapePosition);
-		animate(getSpaceshipPosition(), SPACESHIP_START, 1000, setSpaceshipPosition);
-	}, false);
+	var animateByMouse;
+	(function() {
+		var beginning;
+		animateByMouse = function(mouseY) {
+			if (!beginning) beginning = mouseY;
 
+			var propertyStart, propertyDestination, nextValue;
+			var progress = mouseY - beginning;
+			var duration = 200;
 
-	document.body.addEventListener('mousemove', function(event) {
-		mouseY = event.clientY;
-		console.log('mouseY: ' + mouseY);
-		if (mouseIsDown) {
-			step(mouseY);
+			// If we're done
+			if (progress > duration) {
+				return;
+			}
+
+			// Animate the bridge
+			propertyStart = BRIDGESHAPE_START;
+			propertyDestination = BRIDGESHAPE_END;
+			nextValue = Math.floor(easeInQuint(progress, propertyStart, propertyDestination - propertyStart, duration));
+			setBridgeshapePosition(nextValue);
+
+			// Animate the spaceship
+			propertyStart = SPACESHIP_START;
+			propertyDestination = SPACESHIP_END;
+			nextValue = Math.floor(easeInQuint(progress, propertyStart, propertyDestination - propertyStart, duration));
+			setSpaceshipPosition(nextValue);
+
+			console.log('--------------');
+			console.log('beginning: ' + beginning);
+			console.log('mouseY: ' + mouseY);
+			console.log('progress: ' + progress);
+			console.log('nextValue: ' + nextValue);
 		}
-	}, false);
+	})();
 
 
 	function animate(propertyStart, propertyDestination, duration, drawFrame, onFinish) {
@@ -154,3 +123,42 @@
 	}
 
 
+	// http://upshots.org/actionscript/jsas-understanding-easing
+	/*
+	@t is the current time (or position) of the tween. This can be seconds or frames, steps, seconds, ms, whatever – as long as the unit is the same as is used for the total time [3].
+	@b is the beginning value of the property.
+	@c is the change between the beginning and destination value of the property.
+	@d is the total time of the tween.
+	*/
+	function noEasing (t, b, c, d) {
+		return c * (t / d) + b;
+	}
+
+
+	// http://gsgd.co.uk/sandbox/jquery/easing/
+	function easeOutElastic (t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+	}
+
+
+	function easeInQuint (t, b, c, d) {
+		return c*(t/=d)*t*t*t*t + b;
+	}
+
+
+	// KUDOS: https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
+	function getScrollY() {
+		var supportPageOffset = window.pageXOffset !== undefined;
+		var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+		return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+	}
+
+
+	// Start listening
+	addListeners();
+
+})();
