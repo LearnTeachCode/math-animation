4d6b3a05751a37103bcda6348a2e66e153b4c893
Jim Thoburn <jim@jimthoburn.com>
2016-09-11 21:54:47 -0700

    Make animation based on scroll position.

--- spaceship/animation.js
+++ spaceship/animation.js
@@ -1,13 +1,20 @@
 (function() {
 
-	var spaceship = document.getElementById('spaceship');
+	var header      = document.querySelector('header');
+	var refresh     = document.querySelector('.refresh');
 	var bridgeshape = document.getElementById('bridgeshape');
+	var spaceship   = document.getElementById('spaceship');
+
+	var startPosition = 0;
 
 	var SPACESHIP_START = 99;
-	var SPACESHIP_END = 154;
+	var SPACESHIP_END   = 198;
 
 	var BRIDGESHAPE_START = 80;
-	var BRIDGESHAPE_END = 130;
+	var BRIDGESHAPE_END   = 170;
+
+	var REFRESH_MARGIN_START = -176;
+	var REFRESH_MARGIN_END   = -90;
 
 
 	var setSpaceshipPosition, getSpaceshipPosition;
@@ -34,14 +41,39 @@
 		}
 	})();
 
+	var setRefreshMarginPosition, getRefreshMarginPosition;
+	(function() {
+		var currentValue;
+		setRefreshMarginPosition = function(newValue) {
+			currentValue = newValue;
+			refresh.style.marginBottom = newValue + 'px';
+		}
+		getRefreshMarginPosition = function() {
+			return currentValue;
+		}
+	})();
+
+
+	// Scroll down enough for the refresh to be out of view
+
+	startPosition = refresh.offsetHeight;
+
+	window.addEventListener('load', function() {
+		setTimeout(function() {
+			window.scroll(0, startPosition + REFRESH_MARGIN_START);
+		}, 1);
+	}, false);
+
 
 	// Add listeners
+	/*
 	(function() {
 		var mouseIsDown = false;
 
 		document.body.addEventListener('mousedown', function(event) {
 			mouseIsDown = true;
 		}, false);
+
 		document.body.addEventListener('mouseup', function(event) {
 			mouseIsDown = false;
 
@@ -52,11 +84,91 @@
 
 		document.body.addEventListener('mousemove', function(event) {
 			var mouseY = event.clientY;
-			console.log('mouseY: ' + mouseY);
+			// console.log('mouseY: ' + mouseY);
 			if (mouseIsDown) {
 				animateByMouse(mouseY);
 			}
 		}, false);
+	})();
+	*/
+
+	(function() {
+		var scrollResetAnimation;
+		var autoScrolling = false;
+		window.addEventListener('scroll', function(e) {
+			var scrollY = getScrollY();
+			//console.log('scrollY: ' + scrollY);
+
+			if (autoScrolling) return;
+
+			if (scrollY <= 0) {
+				// Animate things back to the top position
+				/*
+				animate(getBridgeshapePosition(), BRIDGESHAPE_START, 1000, setBridgeshapePosition);
+				animate(getSpaceshipPosition(), SPACESHIP_START, 1000, setSpaceshipPosition);
+				animate(getRefreshMarginPosition(), REFRESH_MARGIN_START, 1000, setRefreshMarginPosition);
+
+				setTimeout(function() {
+					if (scrollResetAnimation) scrollResetAnimation.stop();
+					scrollResetAnimation = animate(scrollY, startPosition, 1000, function(nextValue) {
+						window.scroll(0, nextValue);
+					}, function() {
+						setTimeout(function() {
+							autoScrolling = false;
+						}, 500);
+					});
+					autoScrolling = true;
+				}, 1000);
+				*/
+
+			} else if (scrollY < startPosition) {
+				animateByScroll(scrollY * 2);
+			}
+
+		}, false);
+	})();
+
+	var animateByScroll;
+	(function() {
+		var beginning = startPosition * -1;
+		animateByScroll = function(scrollY) {
+			//console.log('scrollY: ' + scrollY);
+
+			var propertyStart, propertyDestination, nextValue;
+			var progress = (scrollY * -1) - beginning;
+			var duration = 300;
+
+			// If we're done
+			if (progress > duration) {
+				return;
+			}
+
+			// Animate the bridge
+			propertyStart = BRIDGESHAPE_START;
+			propertyDestination = BRIDGESHAPE_END;
+			nextValue = Math.floor(easeInQuint(progress, propertyStart, propertyDestination - propertyStart, duration));
+			setBridgeshapePosition(nextValue);
+
+			// Animate the refresh container’s margin too, so the list will appear to be the same distance away.
+			propertyStart = REFRESH_MARGIN_START;
+			propertyDestination = REFRESH_MARGIN_END;
+			nextValue = Math.floor(easeInQuint(progress, propertyStart, propertyDestination - propertyStart, duration));
+			setRefreshMarginPosition(nextValue);
+
+			// Animate the spaceship
+			propertyStart = SPACESHIP_START;
+			propertyDestination = SPACESHIP_END;
+			nextValue = Math.floor(easeInQuint(progress, propertyStart, propertyDestination - propertyStart, duration));
+			setSpaceshipPosition(nextValue);
+
+			/*
+			console.log('--------------');
+			console.log('beginning: ' + beginning);
+			console.log('mouseY: ' + mouseY);
+			console.log('progress: ' + progress);
+			console.log('nextValue: ' + nextValue);
+			*/
+		}
 	})();
 
 
@@ -97,30 +209,45 @@
 
 
 	function animate(propertyStart, propertyDestination, duration, drawFrame, onFinish) {
+		console.log('animate');
 		var beginning;
+		var stopAnimation = false;
 
 		function step(timestamp) {
 			if (!beginning) beginning = timestamp;
 
 			var progress = timestamp - beginning;
 
+			if (progress < duration && !stopAnimation) {
+				requestAnimationFrame(step);
+			} else {
+				if (stopAnimation) {
+					console.log('animation stopped');
+				}
+				if (onFinish) onFinish();
+				return;
+			}
+
 			var nextValue = Math.floor(easeInQuint(progress, propertyStart, propertyDestination - propertyStart, duration));
 
 			drawFrame(nextValue);
 
+			/*
 			console.log('--------------');
 			console.log('beginning: ' + beginning);
 			console.log('timestamp: ' + timestamp);
 			console.log('progress: ' + progress);
 			console.log('nextValue: ' + nextValue);
-
-			if (progress < duration) {
-				requestAnimationFrame(step);
-			} else {
-				if (onFinish) onFinish();
-			}
+			*/
 		}
 		requestAnimationFrame(step);
+
+		return {
+			stop: function() {
+				console.log('stop animation');
+				stopAnimation = true;
+			}
+		}
 	}
 
 
